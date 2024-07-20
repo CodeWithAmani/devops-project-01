c9e5e2109b8eefb1639f11324463246765f8b295
abel <abelamani1@hotmail.com>
2024-07-19 22:33:40 -0400

    update code

--- terraform_code/vs1-ec2.tf
+++ terraform_code/vs1-ec2.tf
@@ -1,36 +1,33 @@
 provider "aws" {
+  profile = "cloudadmin"
   region = "us-east-1"
 }
 
-resource "aws_instance" "vs1-ec2" {
-    ami = "ami-022e1a32d3f742bd8"
-    instance_type = "t2.micro"
-    key_name = "dpp"
-    security_groups = [ "vs1-ec2-sg" ]
-}
-
-resource "aws_security_group" "vs1-ec2-sg" {
-  name        = "vs1-ec2-sg"
-  description = "use SSH Access"
+resource "aws_security_group" "vs1_ec2_sg" {
+  name        = "vs1ec2-sg"
+  description = "Security group for vs1-ec2"
   
   ingress {
-    description      = "Shh access"
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-    }
-
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
+  
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "vs1_ec2" {
+  ami           = "ami-01fccab91b456acc2" # Amazon Linux 2 AMI ID (replace with the AMI ID for your region)
+  instance_type = "t2.micro"
+  security_groups = [aws_security_group.vs1_ec2_sg.name]
 
   tags = {
-    Name = "vs1-ec2-port"
-
+    Name = "vs1-ec2"
   }
 }
